076dae02b767d6e9add26e5fc4b56c249533badc
vavellar <victor.avellar01@gmail.com>
2023-04-14 20:26:34 -0300

    feat: creating first version of login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,59 @@
 <template>
   <div class="login">
-    <h1>This is an about page</h1>
-    <form class="login__form" @submit.prevent="login">
+      <img
+          src="../assets/images/locaweb.svg"
+          alt="logo"
+      >
+      <form class="login__form" @submit.prevent="login">
         <h2>Entre na sua conta</h2>
         <h3>Para acessar sua conta informe seu e-mail e senha</h3>
-        <input type="text" label="teste">
+        <Input
+          type="email"
+          class="login__input"
+          label="E-mail"
+          placeholder="Seu e-mail"
+          v-model="email"
+        />
+        <Input
+          class="login__input"
+          label="Senha"
+          placeholder="Sua senha"
+          v-model="password"
+        />
+        <button type="submit">Fazer login</button>
     </form>
+    <h3>Ainda não tem uma conta? <a href="/register">Cadastre-se</a></h3>
   </div>
 </template>
 
 <script setup lang="ts">
+import {ref} from "vue";
+import Input from "@/components/Input.vue";
+
+const email = ref(null)
+const password = ref(null)
 function login() {
-    console.log('teste')
+    console.log(email.value)
+    console.log(password.value)
 }
 </script>
 
 <style lang="scss">
+.login {
+  &__form {
+    background-color: #FFFFFF;
+    padding: 2.4rem;
+    margin: 2rem 0;
+    border: 1px solid #E6EAF2;
+    border-radius: 5px;
+  }
+  &__input {
+    margin-top: 1rem;
+  }
+}
 @media (min-width: 1024px) {
   .login {
+    justify-content: center;
     flex-direction: column;
     min-height: 100vh;
     display: flex;
