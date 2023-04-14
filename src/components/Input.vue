8e3dba9b4eeccfcb3d454e03967ca2f121f7586c
vavellar <victor.avellar01@gmail.com>
2023-04-14 20:26:13 -0300

    feat: creating input component

---
+++ src/components/Input.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="input__wrapper">
+        <label class="input__label">{{ label }}</label>
+        <input
+            class="input__element"
+            :value="modelValue"
+            :type="type"
+            :placeholder="placeholder"
+            @input="$emit('update:modelValue', $event.target.value)"
+        />
+    </div>
+</template>
+
+<script setup>
+defineEmits(['update:modelValue'])
+defineProps(['modelValue', 'label', 'placeholder', 'type'])
+
+</script>
+
+<style lang="scss">
+.input {
+  &__wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__label {
+    margin-bottom: 2px;
+  }
+
+  &__element {
+    padding: 16px;
+    border: 1px solid rgba(81, 93, 116, 0.5);
+    border-radius: 5px;
+  }
+}
+</style>
