cfbf5c25581c2c186f72348f706708d705c0f2eb
vavellar <victor.avellar01@gmail.com>
2023-04-16 16:23:02 -0300

    feat: creating plans array in utils folder to make components smaller

---
+++ src/utils/plans.ts
@@ -0,0 +1,60 @@
+export const plans = [
+    {
+        title: 'Hospedagem 1',
+        value: 'Grátis',
+        badge: false,
+        info: 'Ideal para quem está começando',
+        text: 'Seu site em servidores nos estados unidos',
+        options: [
+            'Servidores em nossos data center americanos;',
+            'ASP, ASP.NET 2.0/3.0/3.5/ e 4.0/4.5/4.5.1/4.5.2 (medium trust) ou PHP 5.3, 5.4, 5.5, 5.6 e 7.0;',
+            '1 usuário de FTP para upload ou download;',
+            '1 contas de e-mails profissionais;',
+            '1 subdomínio gratuito;'
+        ],
+    },
+    {
+        title: 'Hospedagem 2',
+        value: 'R$ 499/mês',
+        badge: true,
+        info: 'Ideal para site com mais de 30k de visitas',
+        text: 'Seu site em servidores no Brasil',
+        options: [
+            'Servidores em nosso de São Paulo;',
+            'ASP, ASP.NET 2.0/3.0/3.5/ e 4.0/4.5/4.5.1/4.5.2 (medium trust) ou PHP 5.3, 5.4, 5.5, 5.6 e 7.0;',
+            '10 usuário de FTP para upload ou download;',
+            '30 contas de e-mails profissionais;',
+            '10 subdomínio gratuito;'
+        ],
+    },
+    {
+        title: 'Hospedagem 3',
+        value: 'R$ 999/mês',
+        badge: false,
+        info: 'Ideal para site com mais de 60k de visitas',
+        text: 'Seu site em servidores no Brasil',
+        options: [
+            'Servidores em nosso de São Paulo;',
+            'ASP, ASP.NET 2.0/3.0/3.5/ e 4.0/4.5/4.5.1/4.5.2 (medium trust) ou PHP 5.3, 5.4, 5.5, 5.6 e 7.0;',
+            'Ilimitados usuário de FTP para upload ou download;\n',
+            'Ilimitadas contas de e-mails profissionais;',
+            '50 subdomínio gratuito'
+        ],
+    }
+]
+
+export const availableApps = [
+    'WordsPress;',
+    'Drupal;',
+    'Entre outros;'
+]
+
+export const benefits = [
+    'Webmail RoundCube;',
+    'AntiSpam',
+    'Painel de gerenciamento de DNS;',
+    'Painel de controle web;',
+    'Configurações de PHP personalizáveis;',
+    'Certificado SSL Grátis;',
+    'Transferência ilimitada.'
+]
