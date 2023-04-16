d3f86cf6e29b48e1ac91a5218a2281f533b4fe68
vavellar <victor.avellar01@gmail.com>
2023-04-16 16:21:01 -0300

    feat: adding disable state and variant prop to button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,16 +1,31 @@
 <template>
-    <button :type="type" class="button" @click="handleClick">{{ text }}</button>
+    <button
+        :type="type"
+        :class="['button', { 'button-disabled': isDisabled }, `button-${variant}`]"
+        @click="handleClick"
+        :disabled="isDisabled"
+        text="">
+        {{ text }}
+    </button>
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
 const emit = defineEmits(['click'])
 defineProps({
     text: {
         type: String,
+        required: true
     },
     type: {
         type: String
+    },
+    variant: {
+        type: String,
+        default: 'contained'
+    },
+    isDisabled: {
+        type: Boolean,
+        default: false
     }
 });
 function handleClick() {
@@ -20,19 +35,40 @@ function handleClick() {
 
 <style lang="scss" scoped>
 .button {
-  background-color: #F30168;
   padding: 24px;
   display: flex;
   justify-content: center;
   width: 100%;
   border-radius: 5px;
-  color: white;
-  border: none;
   cursor: pointer;
+  border: none;
 
-  &:hover {
-    background-color: #e66691;
-    transition: 0.5s;
+  &-contained {
+    background-color: #F30168;
+    color: white;
+
+      &:not([disabled]):hover {
+          background-color: #e66691;
+          transition: 0.5s;
+      }
   }
+
+  &-outlined {
+    background-color: transparent;
+    color: black;
+    border: 1px solid black;
+
+      &:not([disabled]):hover {
+          background-color: #f6f6f6;
+          transition: 0.5s;
+      }
+  }
+
+  &-disabled {
+      background: #E5E5E5;
+      color: #BBBBBB;
+      cursor: not-allowed;
+  }
+
 }
 </style>
